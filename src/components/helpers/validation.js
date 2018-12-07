f6bc4e6e55909013727d213fa93ed2ead887bfbb
smwbtech <smwb.tech@gmail.com>
2018-12-07 11:19:34 +0300

    Update validation.js
    *Переработан метод валидации. Функция должна получать объект для валидации и название коллекции, откуда брать параметры валидации для свойств объекта.
    * Добавлена коллекция для dataObject

--- src/components/helpers/validation.js
+++ src/components/helpers/validation.js
@@ -1,43 +1,106 @@
-export function configValidation(obj) {
+/**
+ * Validation for configuration and data objects,
+ * This function will be executed with void result
+ * or throw Error or console.warn if something goes wrong.
+ * @param  {Object} obj             - objet to validate
+ * @param  {String} validationObj   - name of object to validate
+ * @return void
+ */
 
-    let check = {
-        status: true,
-        message: ''
-    };
+export default function(obj, validationObj) {
+
+    let check = true;
+    let properties = validObj[validationObj];
 
     if(typeof obj !== 'object' || Array.isArray(obj)) {
         throw new Error('[vue-flash-msg]: cofig must be an Object');
     }
 
     for(const prop in obj) {
-        check.status = typeof obj[prop] === properties[prop].type && properties[prop].pattern ? properties[prop].pattern.test(obj[prop]) : true;
-        if(!check.status) throw new Error(properties[prop].message);
+        if(properties[prop]) {
+            switch (properties[prop].type) {
+                case 'object':
+                check = typeof obj[prop] === properties[prop].type && !Array.isArray(obj[prop]);
+                break;
+                case 'array':
+                check = Array.isArray(obj[prop]);
+                break;
+                default:
+                check = properties[prop].pattern ? properties[prop].pattern.test(obj[prop]) : true && typeof obj[prop] === properties[prop].type;
+            }
+            if(!check) {
+                let message = `[vue-flash-message] Property "${prop}" of the "${validationObj}" should be a "${properties[prop].type}"${properties[prop].message ? ` ${properties[prop].message}` : ''}`;
+                throw new Error(message);
+            }
+        }
     }
 }
 
-const properties = {
-    name: {
-        type: 'string',
-        pattern: /^.+$/ig,
-        message: '[vue-flash-message] Property "name" of the "config object" should be a "Sring" and not empty'
+const validObj = {
+    // Validation of configObject params
+    configObject: {
+        name: {
+            type: 'string',
+            pattern: /^.+$/ig,
+            message: 'and not empty'
+        },
+        tag: {
+            type: 'string',
+            pattern: /^.+$/ig,
+            message: 'and not empty'
+        },
+        time: {
+            type: 'number',
+        },
+        icon: {
+            type: 'boolean',
+        },
+        strategy: {
+            type: 'string',
+            pattern: /^(single|multiple)$/ig,
+            message: 'and be equal to "single" or "multiple"'
+        },
     },
-    tag: {
-        type: 'string',
-        pattern: /^.+$/ig,
-        message: '[vue-flash-message] Property "tag" of the "config object" should be a "Sring" and not empty'
-    },
-    time: {
-        type: 'number',
-        message: '[vue-flash-message] Property "time" of the "config object" should be a "Number"'
-    },
-    icon: {
-        type: 'boolean',
-        message: '[vue-flash-message] Property "icon" of the "config object" should be a "Boolean"'
-    },
-    strategy: {
-        type: 'string',
-        pattern: /^(single|multiple)$/ig,
-        message: '[vue-flash-message] Property "strategy" of the "config object" should be a "Sring" and be equal to "single" or "multiple"'
-    },
-
+    // Validation for message data object
+    dataObject: {
+        status: {
+            type: 'string'
+        },
+        title: {
+            type: 'string'
+        },
+        message: {
+            type: 'string'
+        },
+        time: {
+            type: 'number',
+        },
+        icon: {
+            type: 'boolean'
+        },
+        clickable: {
+            type: 'boolean'
+        },
+        flashMessageStyle: {
+            type: 'object'
+        },
+        iconStyle: {
+            type: 'object'
+        },
+        contentStyle: {
+            type: 'object'
+        },
+        titleStyle: {
+            type: 'object'
+        },
+        textStyle: {
+            type: 'object'
+        },
+        mounted: {
+            type: 'function'
+        },
+        destroyed: {
+            type: 'function'
+        }
+    }
 }
