2c3d0e2718d389391910480062f31963bbce1e79
smwbtech <smwb.tech@gmail.com>
2018-12-08 00:25:27 +0300

    Update message.js
    *Объект сообщения приходит через props, как messageObj
    * Обновлены все методы, связанные с messageObj

--- src/components/mixins/message.js
+++ src/components/mixins/message.js
@@ -2,44 +2,28 @@ export function createMessageMixin(config) {
     //If user choosed 'bus' strategy or didn't pass any args
         return {
 
+            props: {
+                messageObj: {
+                    type: Object,
+                    required: true
+                }
+            },
+
             data() {
                 return {
-                    id: undefined, // message id
-                    timeoutId: undefined, // id that will be returned by setTimeout() function
-                    time: config.time, // defualt time for timeOut function
-                    status: '', // message status: available 'error', 'warning', 'success', 'info'
-                    title: '', //  message title
-                    message: '', // message text
-                    icon: '', // display icon
-                    clickable: true, //can be removed by click
-                    blockClass: null, // class of flashMessage block
-                    iconClass: null, // class for icons
-                    contentClass: null, // class for content block
-                    mounted: null, // mounted callback function
-                    destroyed: null // destroyed hook callback function
+                    timeoutId: undefined // id that will be returned by setTimeout() function
                 }
             },
 
             computed: {
-                /**
-                 * Retutn a title of the message
-                 * @return {String}     - user's title or default title that is the same as status
-                 */
-                getTitle(){
-                    return this.title ? this.title : this.status.toUpperCase() || 'HEY, LOOK AT ME';
-                },
-
-                getMessage() {
-                    return this[config.name].message;
-                },
 
                 classObj() {
                     return {
-                        '_vue-flash-msg-body_success': this.status === 'success',
-                        '_vue-flash-msg-body_error': this.status === 'error',
-                        '_vue-flash-msg-body_warning': this.status === 'warning',
-                        '_vue-flash-msg-body_info': this.status === 'info',
-                        '_vue-flash-msg-body_unclickabe': !this.clickable,
+                        '_vue-flash-msg-body_success': this.messageObj.status === 'success',
+                        '_vue-flash-msg-body_error': this.messageObj.status === 'error',
+                        '_vue-flash-msg-body_warning': this.messageObj.status === 'warning',
+                        '_vue-flash-msg-body_info': this.messageObj.status === 'info',
+                        '_vue-flash-msg-body_unclickabe': !this.messageObj.clickable,
                         '_vue-flash-msg-body': true,
                     }
                 }
@@ -48,32 +32,12 @@ export function createMessageMixin(config) {
             methods: {
 
                 /**
-                 * Set component data
-                 * @param {Object} data     - message's data object
-                 */
-                setData(data) {
-                    for(let prop of Object.keys(data)) {
-                        switch (prop) {
-                            case 'clickable':
-                                this.clickable = data.clickable === undefined ? true : false;
-                                break;
-                            case 'time':
-                                this[prop] = data[prop] ? data[prop] : config.time;
-                                break;
-                            default:
-                                this[prop] = data[prop];
-                        }
-                    }
-                    this.timeoutId = this.setTimeout(this.clearData.bind(this, false), data.time ? data.time : this.time);
-                },
-
-                /**
                  * Custom set timeout with default time
                  * @param {Function} callback           - callback function
                  * @param {Number}   [time=this.time]   - time in mileseconds
                  * return {Number}                      - timeout id
                  */
-                setTimeout(callback, time = this.time) {
+                setTimeout(callback, time = this.messageObj.time) {
                     return setTimeout(callback, time);
                 },
 
@@ -82,33 +46,33 @@ export function createMessageMixin(config) {
                  */
                 clearData(clear = true) {
                         if(this.timeoutId && clear) clearTimeout(this.timeoutId);
-                        this[config.name].$emit('deleteMessage', this.id);
+                        this[config.name].$emit('deleteMessage', this.messageObj.id);
                 },
                 /**
                  * Handle click event
                  */
                 clickHandler() {
-                    if(this.clickable) this.clearData();
+                    if(this.messageObj.clickable) this.clearData();
                 }
             },
 
             // Set up data
             created() {
-                this.setData(this.getMessage);
+                this.timeoutId = this.setTimeout(this.clearData.bind(this, false), this.messageObj.time);
                 this[config.name].$once('clearData', this.clearData);
             },
 
             // Invoke mounted callback function if exist
             mounted() {
-                if(this.mounted && typeof this.mounted === 'function') {
-                    this.mounted();
+                if(this.messageObj.mounted && typeof this.messageObj.mounted === 'function') {
+                    this.messageObj.mounted();
                 }
             },
 
             // Invoke destroyed callback function if exist
             destroyed() {
-                if(this.destroyed && typeof this.destroyed === 'function') {
-                    this.destroyed();
+                if(this.messageObj.destroyed && typeof this.messageObj.destroyed === 'function') {
+                    this.messageObj.destroyed();
                 }
             }
         }
