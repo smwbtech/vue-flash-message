55b11c82ce20711ca8214fd29e604a5f6cb2f2a9
smwbtech <smwb.tech@gmail.com>
2018-12-07 17:31:08 +0300

    Обновлен конфин
    Пакеты cross-env и terser-webpack-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     css: {
         extract: false
     },
     configureWebpack: {
+    mode: 'production',
     plugins: [
-      new UglifyJsPlugin()
+      new TerserPlugin()
     ]
   }
 }
