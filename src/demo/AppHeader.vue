824421a2fbb989756067b8edf6af58ac072b2b1a
smwbtech <smwb.tech@gmail.com>
2019-08-31 11:48:23 +0300

    Static assests for new demo page

---
+++ src/demo/AppHeader.vue
@@ -0,0 +1,105 @@
+<template lang="html">
+	<header>
+		<div class="logo">
+			<img
+				src="/fm-logo.svg"
+				alt="vue flash message logotype"
+				class="logo__img"
+			/>
+			<h1 class="logo__title">vue flash message</h1>
+		</div>
+		<nav class="nav">
+			<ul class="nav-list">
+				<li
+					v-for="item in navItems"
+					:key="item.text"
+					class="nav-list-item"
+				>
+					<a class="nav-list-item__link" :href="item.link">{{
+						item.text
+					}}</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			navItems: [
+				{
+					link: 'https://github.com/smwbtech/vue-flash-message',
+					text: 'github'
+				},
+				{
+					link:
+						'https://www.npmjs.com/package/@smartweb/vue-flash-message',
+					text: 'npm'
+				},
+				{
+					link:
+						'https://github.com/smwbtech/vue-flash-message/issues',
+					text: 'issues'
+				}
+			]
+		};
+	}
+};
+</script>
+
+<style lang="css" scoped>
+
+header {
+    width: 100%;
+    padding-top: 20px;
+    padding-left: calc(var(--side-padding) + var(--column) + var(--gutter));
+    padding-right: calc(var(--side-padding) + var(--column) + var(--gutter));
+    color: var(--app-grey);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-family: 'Baron Neue', sans-serif;
+
+    /* Logo */
+    & .logo {
+        display: flex;
+        align-items: center;
+
+        & .logo__title {
+            margin-left: var(--gutter);
+        }
+    }/* Logo end*/
+
+    /* navigation */
+
+    & .nav {
+
+        & .nav-list {
+            display: flex;
+            align-items: center;
+            padding: 0;
+            margin: 0;
+            list-style: none;
+
+            & .nav-list-item {
+                display: block;
+                margin-left: calc(var(--column) + var(--gutter));
+
+                /* links */
+                & .nav-list-item__link {
+                    color: var(--app-grey);
+                    text-decoration: none;
+                    transition: color .2s ease-in;
+
+                    &:hover {
+                        color:var(--app-blue);
+                    }
+
+                }/* links  end*/
+            }
+        }
+    }/* navigation end */
+}
+</style>
