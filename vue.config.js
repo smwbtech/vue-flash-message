5751209908f2f538e7029be38a5360b7295f513b
smwbtech <smwb.tech@gmail.com>
2019-10-05 15:57:00 +0300

    Update vue.config.js
    * Fixed public path for gh pages

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    css: {
-        extract: false
-    },
-    configureWebpack: {
-    mode: 'production',
-    plugins: [
-      new TerserPlugin()
-    ]
-  }
-}
+	css: {
+		extract: false
+	},
+	configureWebpack: {
+		mode: 'production',
+		plugins: [new TerserPlugin()]
+	},
+	publicPath:
+		process.env.NODE_ENV === 'production' ? '/vue-flash-message/' : '/'
+};
