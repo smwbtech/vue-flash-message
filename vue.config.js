e58897828f6e8241cf129666bd391d14e423feeb
smwbtech <smwb.tech@gmail.com>
2020-02-17 13:38:27 +0300

    Update vue.config.js
    * Fixed path to file

--- vue.config.js
+++ vue.config.js
@@ -20,6 +20,5 @@ module.exports = {
 			]
 		}
 	},
-	publicPath:
-		process.env.NODE_ENV === 'production' ? '/vue-flash-message/' : '/'
+	publicPath: process.env.NODE_ENV === 'build' ? '/vue-flash-message/' : '/'
 };
