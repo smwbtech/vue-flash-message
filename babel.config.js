caab0dabc6e861116f1329bd36cf843e8719e2d3
smwbtech <smwb.tech@gmail.com>
2020-02-17 12:56:33 +0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
+const presets = [
+	[
+		'@vue/babel-preset-app',
+		{
+			useBuiltIns: process.env.NODE_ENV === 'production' ? false : 'usage'
+		}
+	]
+];
+
+if (process.env.NODE_ENV === 'production') presets.unshift(['minify']);
+
 module.exports = {
-	presets: ['@vue/app']
+	presets,
+	plugins: ['transform-vue-jsx', '@babel/plugin-transform-runtime']
 };
