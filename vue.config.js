bcb147df305fea37710c39ba915c8eb6f000557d
Roman Privalov <smwb.tech@gmail.com>
2020-10-03 13:33:53 +0300

    0.7.0-4
    0.7.0-4

--- vue.config.js
+++ vue.config.js
@@ -4,21 +4,21 @@ module.exports = {
 	css: {
 		extract: false
 	},
-	configureWebpack: {
-		mode: 'production',
-		optimization: {
-			minimize: true,
-			minimizer: [
-				new TerserPlugin({
-					sourceMap: false,
-					terserOptions: {
-						ie8: false,
-						safari10: false,
-						ecma: 2015
-					}
-				})
-			]
-		}
-	},
+	// configureWebpack: {
+	// 	mode: 'production',
+	// 	optimization: {
+	// 		minimize: true,
+	// 		minimizer: [
+	// 			new TerserPlugin({
+	// 				sourceMap: false,
+	// 				terserOptions: {
+	// 					ie8: false,
+	// 					safari10: false,
+	// 					ecma: 2015
+	// 				}
+	// 			})
+	// 		]
+	// 	}
+	// },
 	publicPath: process.env.NODE_ENV === 'build' ? '/vue-flash-message/' : '/'
 };
