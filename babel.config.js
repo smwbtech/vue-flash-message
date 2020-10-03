bad41e1535153281ff13adeb4c4e970986cf3e4c
Roman Privalov <smwb.tech@gmail.com>
2020-10-03 20:19:58 +0300

    Tools config
    *Added: tools config

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,29 @@
+// module.exports = {
+//   presets: [
+//     "@vue/cli-plugin-babel/preset",
+//     {
+//       useBuiltIns: process.env.NODE_ENV === "production" ? false : "usage",
+//     },
+//   ],
+// };
 const presets = [
-	[
-		'@vue/cli-plugin-babel/preset',
-		{
-			useBuiltIns: process.env.NODE_ENV === 'production' ? false : 'usage'
-		}
-	]
+  [
+    "@vue/cli-plugin-babel/preset",
+    {
+      useBuiltIns: process.env.NODE_ENV === "production" ? false : "usage",
+    },
+  ],
 ];
 
-if (process.env.NODE_ENV === 'production')
-	presets.unshift([
-		'minify',
-		{
-			builtIns: false
-		}
-	]);
+if (process.env.NODE_ENV === "production")
+  presets.unshift([
+    "minify",
+    {
+      builtIns: false,
+    },
+  ]);
 
 module.exports = {
-	presets,
-	plugins: ['transform-vue-jsx', '@babel/plugin-transform-runtime']
+  presets,
+  plugins: ["transform-vue-jsx", "@babel/plugin-transform-runtime"],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,35 +1,50 @@
+// module.exports = {
+// 	moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+// 	transform: {
+// 		'^.+\\.vue$': 'vue-jest',
+// 		'.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+// 			'jest-transform-stub',
+// 		'^.+\\.jsx?$': 'babel-jest'
+// 	},
+// 	transformIgnorePatterns: ['/node_modules/'],
+// 	moduleNameMapper: {
+// 		'^@/(.*)$': '<rootDir>/src/$1'
+// 	},
+// 	snapshotSerializers: ['jest-serializer-vue'],
+// 	testMatch: [
+// 		'**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+// 	],
+// 	testURL: 'http://localhost/',
+// 	watchPlugins: [
+// 		'jest-watch-typeahead/filename',
+// 		'jest-watch-typeahead/testname'
+// 	],
+// 	collectCoverage: true,
+// 	collectCoverageFrom: [
+// 		'**/src/components/**',
+// 		'!**/src/components/index.js',
+// 		'!**/src/components/fallback/**',
+// 		'!**/App.{vue}',
+// 		'!**/wrapper.js',
+// 		'!**/node_modules/**',
+// 		'!<rootDir>/dist/**',
+// 		'!<rootDir>/src/plugins/**',
+// 		'!<rootDir>/tests/unit/**'
+// 	],
+// 	coverageReporters: ['lcov', 'text-summary']
+// };
+
 module.exports = {
-	moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
-	transform: {
-		'^.+\\.vue$': 'vue-jest',
-		'.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
-			'jest-transform-stub',
-		'^.+\\.jsx?$': 'babel-jest'
-	},
-	transformIgnorePatterns: ['/node_modules/'],
-	moduleNameMapper: {
-		'^@/(.*)$': '<rootDir>/src/$1'
-	},
-	snapshotSerializers: ['jest-serializer-vue'],
-	testMatch: [
-		'**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
-	],
-	testURL: 'http://localhost/',
-	watchPlugins: [
-		'jest-watch-typeahead/filename',
-		'jest-watch-typeahead/testname'
-	],
-	collectCoverage: true,
-	collectCoverageFrom: [
-		'**/src/components/**',
-		'!**/src/components/index.js',
-		'!**/src/components/fallback/**',
-		'!**/App.{vue}',
-		'!**/wrapper.js',
-		'!**/node_modules/**',
-		'!<rootDir>/dist/**',
-		'!<rootDir>/src/plugins/**',
-		'!<rootDir>/tests/unit/**'
-	],
-	coverageReporters: ['lcov', 'text-summary']
+	jest: {
+		moduleFileExtensions: [
+			'js',
+			'json',
+			// tell Jest to handle `*.vue` files
+			'vue'
+		],
+		transform: {
+			// process `*.vue` files with `vue-jest`
+			'.*\\.(vue)$': 'vue-jest'
+		}
+	}
 };
