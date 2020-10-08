33fbda3b794c6fa1c4fce164b1fa3b12624dd6ab
Roman Privalov <smwb.tech@gmail.com>
2020-10-08 17:50:44 +0300

    Changed vue/no-multiple-template-root
    'vue/no-multiple-template-root' => 'warn'

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
 
 	rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'vue/no-multiple-template-root': 'warn'
 	},
 
 	parserOptions: {
