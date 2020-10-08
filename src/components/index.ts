9e3d042bc07382df085b7e8fb1697c3510dd03d6
Roman Privalov <smwb.tech@gmail.com>
2020-10-08 17:51:46 +0300

    Changed: Intext.js => index.ti

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,49 +1,26 @@
-import { createContainerMixin } from './mixins/container.js';
-import { createMessageMixin } from './mixins/message.js';
-import { createEventbusFallback } from './fallback/eventbus-fallback.js';
-import ContainerElem from './Container.vue';
-import FlashMessageElem from './FlashMessage.vue';
+// import { createContainerMixin } from './mixins/container.js';
+// import { createMessageMixin } from './mixins/message.js';
+// import { createEventbusFallback } from './fallback/eventbus-fallback.js';
+// import ContainerElem from './Container.vue';
+// import FlashMessageElem from './FlashMessage.vue';
+import { App } from 'vue';
+import FlashMessagePlugin from './FlashMessagePlugin';
+import FlashMessageContainer from './FlashMessageContainer';
 
-const defaultSettings = {
-	name: 'flashMessage',
-	tag: 'FlashMessage',
-	time: 8000,
-	strategy: 'single'
-};
-
-export default function install(Vue, config = {}, ref) {
+function install(app: App) {
 	if (install.installed) return;
 
 	install.installed = true;
 
-	config = Object.assign(defaultSettings, config);
+	// Set up plugin
+	const $flashMessage = FlashMessagePlugin;
+	app.config.globalProperties.$flashMessage = $flashMessage;
 
-	// Set up Event Bus
-	const EventBus = createEventbusFallback(config, ref);
-	Vue.config.globalProperties[`$${config.name}`] = EventBus;
-	// Extend Container component
-	let Container = Object.assign(ContainerElem, createContainerMixin(config));
-
-	// Extend Flash Message component
-	let FlashMessage = Object.assign(
-		FlashMessageElem,
-		createMessageMixin(config)
-	);
 	//  Set up component
-	Vue.component(config.tag, Container);
-	Vue.component('VueMessageBlock', FlashMessage);
+	app.component('FlashMessage', FlashMessageContainer);
+	// Vue.component('VueMessageBlock', FlashMessage);
 }
 
-const plugin = {
-	install
-};
+install.installed = false;
 
-let GlobalVue = null;
-if (typeof window !== 'undefined') {
-	GlobalVue = window.Vue;
-} else if (typeof global !== 'undefined') {
-	GlobalVue = global.Vue;
-}
-if (GlobalVue) {
-	GlobalVue.use(plugin);
-}
+export default install;
