c14ea1276b1c65f5c42d3a87d235ea989582ad68
Roman Privalov <smwb.tech@gmail.com>
2020-10-15 16:37:40 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,48 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+// import Vue from 'vue';
+// import Vuex from 'vuex';
+// import App from './App.vue';
+// import FlashMessage from '@/components/index.js';
+// import VueHighlightJS from 'vue-highlightjs';
+// import CustomComponent from '@/demo/components/CustomComponent.vue';
+// import Vuescroll from 'vuescroll';
+// import store from '@/demo/store/store.js';
+// import './assets/css/common.css';
+
+// const config = {
+// 	name: 'flashMessage',
+// 	tag: 'FlashMessage',
+// 	time: 8000
+// };
+
+// Vue.config.productionTip = false;
+// Vue.use(FlashMessage, config);
+// Vue.use(VueHighlightJS);
+// Vue.use(Vuescroll, {
+// 	ops: {
+// 		rail: {
+// 			background: '#fff',
+// 			opacity: 0.2,
+// 			keepShow: false
+// 		},
+// 		bar: {
+// 			onlyShowBarOnScroll: true,
+// 			background: '#0092ca'
+// 		}
+// 	}
+// });
+// Vue.use(Vuex);
+
+// Vue.component('CustomComponent', CustomComponent);
+
+// new Vue({
+// 	store,
+// 	render: h => h(App)
+// }).$mount('#app');
+
+import { createApp } from 'vue';
+import FlashMessage from '@/components/index.ts';
 import App from './App.vue';
-import FlashMessage from '@/components/index.js';
-import VueHighlightJS from 'vue-highlightjs';
-import CustomComponent from '@/demo/components/CustomComponent.vue';
-import Vuescroll from 'vuescroll';
-import store from '@/demo/store/store.js';
-import './assets/css/common.css';
 
-const config = {
-	name: 'flashMessage',
-	tag: 'FlashMessage',
-	time: 8000
-};
-
-Vue.config.productionTip = false;
-Vue.use(FlashMessage, config);
-Vue.use(VueHighlightJS);
-Vue.use(Vuescroll, {
-	ops: {
-		rail: {
-			background: '#fff',
-			opacity: 0.2,
-			keepShow: false
-		},
-		bar: {
-			onlyShowBarOnScroll: true,
-			background: '#0092ca'
-		}
-	}
-});
-Vue.use(Vuex);
-
-Vue.component('CustomComponent', CustomComponent);
-
-new Vue({
-	store,
-	render: h => h(App)
-}).$mount('#app');
+const app = createApp(App);
+app.use(FlashMessage);
+app.mount('#app');
